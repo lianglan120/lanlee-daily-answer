47b932fbc37bc5c8fd1f0a7af3d5665bf78c99a1
lianglan120 <liangtcd@gmail.com>
2025-06-24 22:28:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ async function loadQuoteAndBackground() {
   const quotes = await response.json();
   const quote = quotes[Math.floor(Math.random() * quotes.length)];
 
-  const totalImages = 62;
+  const totalImages = 130;
   const randomImage = Math.floor(Math.random() * totalImages) + 1;
   const imagePath = `images/quote_image_${randomImage}.jpg`;
 
